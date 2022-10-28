4ddcc8f64bc998cee3af606b9e78c82f7f78022c
PaidUser1 <116077835+PaidUser1@users.noreply.github.com>
2022-10-28 13:44:20 +0500

    Update and rename JAN.py to V.py

--- V.py
+++ V.py
@@ -1,14 +1,5 @@
 #coding=utf-8
-import sys,time
-from platform import uname
-from os import path,system
-arch=uname().machine.lower()
-if "aarch" in arch:
-    arch="aarch"
-    print('\033[1;32m\nCongratulatings! Your Deviec Support This Tools');time.sleep(1)
-else:
-    print('\033[1;31m\nSorry System not support this tools');sys.exit()
- 
+
 try:
     if sys.argv[1]=='update':
         system('cd $HOME && cd AKING && rm -f *')
